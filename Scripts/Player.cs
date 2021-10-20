137f457ca8dd2c2195d1971a9fe526b6fb0ebbe1
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-10-20 21:33:07 +0300

    keep the bat in boundaries

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,9 +7,13 @@ public class Player : RigidBody2D
     private const float SPEED = 750.0f;
     private bool _isHoldingLeft = false;
     private bool _isHoldingRight = false;
+    private float _leftBoundary;
+    private float _rightBoundary;
 
     public override void _Ready()
     {
+        _leftBoundary = GetNode<Sprite>("Sprite").Texture.GetWidth() / 2.0f;
+        _rightBoundary = GetViewportRect().Size.x - _leftBoundary;
     }
 
     public override void _Process(float delta)
@@ -45,10 +49,18 @@ public class Player : RigidBody2D
         if (_isHoldingLeft)
         {
             Translate(new Vector2(-SPEED * delta, 0.0f));
+            if (Position.x < _leftBoundary)
+            {
+                Position = new Vector2(_leftBoundary, Position.y);
+            }
         }
         else if (_isHoldingRight)
         {
             Translate(new Vector2(SPEED * delta, 0.0f));
+            if (Position.x > _rightBoundary)
+            {
+                Position = new Vector2(_rightBoundary, Position.y);
+            }
         }
         
     }
